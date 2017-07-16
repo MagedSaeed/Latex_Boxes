e8b71119bc977c32463ac9c248924549061872f4
MagedSaeed <msmj2015@gmail.com>
2017-07-16 21:35:54 +0300

    Shortcut API
    This a useless class for the time being. Hoever, It may be useful later.

--- JS/javascript.js
+++ JS/javascript.js
@@ -979,23 +979,44 @@ elem.style.marginTop = marginTop + 'px';
 
 }
 
-// code to add keyboard shortcuts:
-// define a handler
-// function kyb_shortcuts(e) {
 
-//     // this would test for whichever key is 40 and the ctrl key at the same time
-//     if (e.ctrlKey && e.keyCode == 66) {
-//         // call your function to do the thing
-//         add('');
-//     }
+// code to add keyboard shortcuts..,,
+function doc_keyUp(e) {
 
-//      else if (e.ctrlKey && e.keyCode == 16 && e.keyCode == 67) {
-//         // call your function to do the thing
-//         copyToClipboard();
-//     }
-// }
-// // register the handler 
-// document.addEventListener('keyup', kyb_shortcuts, false);
+    // this would test for whichever key is 40 and the ctrl key at the same time
+    if (e.keyCode == 78 && e.altKey) {
+        // call your function to do the thing
+        add('');
+    }
+
+    else if (e.keyCode == 88 && e.altKey) {
+        // call your function to do the thing
+        download_xml();
+    }
+    else if (e.keyCode == 84 && e.altKey) {
+        // call your function to do the thing
+        download_text();
+    }
+    else if (e.keyCode == 85 && e.altKey) {
+        // call your function to do the thing
+      document.getElementById('myFile').click();
+    }
+
+    else if (e.keyCode == 67 && e.altKey) {
+        // call your function to do the thing
+      copyToClipboard();
+    }
+
+    else if (e.keyCode == 80 && e.altKey) {
+        // call your function to do the thing
+      preview_all();
+    }
+}
+// register the handler 
+document.onkeyup = doc_keyUp ;
+
+
+
 
 
 
